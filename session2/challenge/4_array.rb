8affd1fde28f444aa55c61af5c5179c90f6098c3
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-09 14:31:38 +0100

    S2C4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,17 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+#Preffered solution
+def get_squares(array)
+  array.select { |n| array.include?(n**2) }.sort
+end
+
+#Solution I first came up with
+def get_squares2(array)
+  new_arr = []
+  array.each do |item|
+    new_arr << item if array.include?(item**2)
+  end
+  new_arr.sort
+end
