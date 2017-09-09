0714494be25b69d7978f3aa4df3a13b8c8a0fe59
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-09 14:25:22 +0100

    S1C7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  answer = ""
+  string.length.times do |i|
+    answer += string[i] if string[i-1] == "r" || string[i-1] == "R"
+  end
+  answer
 end
