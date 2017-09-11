0ee8456859686f8e425be72694c507520b59fd09
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-11 15:16:48 +0100

    S3C2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,31 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+#First attempt solution
+#helper method
+def evens_upto_i(n)
+  [] if n == 1
+  array = [*1..n]
+  array.map { |i| array.delete(i) if i.odd? }
+  array
+end
+
+def staircase(n)
+  staircase = Hash.new
+  (1..n).each do |i|
+    staircase[i] = evens_upto_i(i) if i.odd?
+  end
+  staircase
+end
+
+#Answer
+def staircase(n)
+  to_return = {}
+  (1..n).each do |crnt_size|
+    next if crnt_size.even?
+    all   = Array.new(crnt_size) { |i| i + 1 }
+    evens = all.select { |i| i.even? }
+    to_return[crnt_size] = evens
+  end
+  to_return
+end
